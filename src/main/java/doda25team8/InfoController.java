a7881e795248c57cc1e5d8f57013c5b1acaaff53
[Yuchen Sun] <[syc617185828@gmail.com]>
2025-11-15 12:58:14 +0100

    can use getVersion() from lib-version now

---
+++ src/main/java/doda25team8/InfoController.java
@@ -0,0 +1,8 @@
+package doda25team8;
+import doda25team8.version.VersionUtil;
+
+public class InfoController {
+    public static void main(String[] args) {
+        System.out.println("Library version: " + VersionUtil.getVersion());
+    }
+}
