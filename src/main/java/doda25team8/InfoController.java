4049fe98fdd7569b5bb912ac82d01db33dd56f1d
Hendrik Lambert <hendrik@lambert.biz>
2025-11-19 19:35:45 +0100

    Moved sms-checker frontend to folder and fixed pom. Library dependency needs to be added later on

--- src/main/java/doda25team8/InfoController.java
+++
@@ -1,8 +0,0 @@
-package doda25team8;
-import doda25team8.version.VersionUtil;
-
-public class InfoController {
-    public static void main(String[] args) {
-        System.out.println("Library version: " + VersionUtil.getVersion());
-    }
-}
